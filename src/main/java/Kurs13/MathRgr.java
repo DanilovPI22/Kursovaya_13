b1c257f711fb2f717cf81735c20a91a8f8c98c43
Kolesova222 <lera3262@gmail.com>
2021-04-27 21:54:50 +0500

    Была создана заготовка для будущего класса расчётов

--- src/main/java/Kurs13/MathRgr.java
+++ src/main/java/Kurs13/MathRgr.java
@@ -1,5 +1,38 @@
 package Kurs13;
 
-public class MathRgr {
+import java.io.IOException;
 
-}
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="Calc", urlPatterns="/JavaCalc")
+public class MathRgr extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestCalc Calc = RequestCalc.fromRequestParameters(request);
+		Calc.setAsRequestAttributesAndCalculate(request);
+		 
+		request.getRequestDispatcher("/Results.jsp").forward(request, response);
+		
+	}
+	
+	private static class RequestCalc {
+	
+						
+		private RequestCalc (String first, String second) {
+		
+			}
+		
+		public static RequestCalc fromRequestParameters(HttpServletRequest request) {
+			return new RequestCalc(
+		
+			}
+				
+		public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
+			
+		}}
+		
+	}
