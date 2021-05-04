9cd4dfa46c197b92d71e2b380d6d46af1613acef
Evelina-Yusupova <evelina120202@mail.ru>
2021-05-04 21:23:18 +0500

    Добавила абстрактный класс

---
+++ src/main/java/Kurs13/Abstract.java
@@ -0,0 +1,53 @@
+package Kurs13;
+
+import java.io.IOException;
+
+public abstract class Abstract {
+static double db1;
+public abstract void Abs();
+
+protected double Math2(String shrif) {
+
+return 0;
+}
+
+protected int Math1(String slovo, String vysota) {
+
+return 0;
+}
+
+protected double Math3(String svet) throws IOException {
+
+return 0;
+}
+
+protected double Math4(String diod) {
+
+return 0;
+}
+
+protected int Math5(String vys, String shir) {
+
+return 0;
+}
+
+protected double Math6(String koef1) {
+
+return 0;
+}
+
+protected double Math7(String koef2) {
+
+return 0;
+}
+
+protected double Math8(String koef3) {
+
+return 0;
+}
+
+protected double Math9(String koefv1) {
+
+return 0;
+}
+}
