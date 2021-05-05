7e94a746d711a8d289f16f98e6cc4b8f297aa3f2
Evelina-Yusupova <evelina120202@mail.ru>
2021-05-05 17:30:39 +0500

    Добавила сохранение настроек администратора

---
+++ src/main/java/Kurs13/AdminSettings.java
@@ -0,0 +1,51 @@
+package Kurs13;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdminSettings extends HttpServlet {
+    protected static String shriftadmin;
+    protected static String newvalue;
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        RequestDispatcher rs = request.getRequestDispatcher("/New.jsp");
+        rs.include(request, response);
+
+         newvalue = request.getParameter("adminput");
+
+
+         setAdm(newvalue);
+
+
+    } 
+
+    protected static void setAdm(String usf) throws IOException {
+
+        File file = new File("AdmSettings.txt");
+        if (file.exists() == false) {
+            file.createNewFile();
+            System.out.println("Успешно");
+        }
+
+        PrintWriter pw = new PrintWriter(file);
+
+        pw.println(usf);
+
+        pw.flush();
+        pw.close();
+
+     System.out.println("Успешно");
+
+}
+}
