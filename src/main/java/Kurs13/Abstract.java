9276422a534ba6989288efe5a603cb6aee8d6103
Evelina-Yusupova <evelina120202@mail.ru>
2021-05-12 16:55:22 +0500

    Дополнение абстрактного класса

--- src/main/java/Kurs13/Abstract.java
+++ src/main/java/Kurs13/Abstract.java
@@ -4,50 +4,78 @@ import java.io.IOException;
 
 public abstract class Abstract {
 static double db1;
+static double back;
+static int backi;
 public abstract void Abs();
 
-protected double Math2(String shrif) {
+public static double Parse (String u) {
 
-return 0;
+if (!u.isEmpty()) {
+back = Double.parseDouble(u);
 }
 
-protected int Math1(String slovo, String vysota) {
 
-return 0;
+return back;
+
 }
 
-protected double Math3(String svet) throws IOException {
+public static int Parseint (String u) {
 
-return 0;
+if (!u.isEmpty()) {
+backi = Integer.parseInt(u);
 }
 
+
+return backi;
+
+}
 protected double Math4(String diod) {
-
-return 0;
-}
-
-protected int Math5(String vys, String shir) {
-
-return 0;
-}
-
-protected double Math6(String koef1) {
-
-return 0;
-}
-
-protected double Math7(String koef2) {
-
-return 0;
-}
-
-protected double Math8(String koef3) {
 
 return 0;
 }
 
 protected double Math9(String koefv1) {
 
+return 0;
+}
+
+protected int Math1(String slovo, String vysota) {
+// TODO Auto-generated method stub
+return 0;
+}
+
+protected String getKoef() throws IOException {
+// TODO Auto-generated method stub
+return null;
+}
+
+protected double Math2(String shrif) {
+// TODO Auto-generated method stub
+return 0;
+}
+
+protected double Math3(String svet) throws IOException {
+// TODO Auto-generated method stub
+return 0;
+}
+
+protected int Math5(String vys, String shir) {
+// TODO Auto-generated method stub
+return 0;
+}
+
+protected double Math6(String koef1) {
+// TODO Auto-generated method stub
+return 0;
+}
+
+protected double Math7(String koef2) {
+// TODO Auto-generated method stub
+return 0;
+}
+
+protected double Math8(String koef3) {
+// TODO Auto-generated method stub
 return 0;
 }
 }
