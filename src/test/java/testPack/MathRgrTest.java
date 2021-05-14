e354c756df71e91dede839e570e61df3ad48d89a
Kolesova222 <lera3262@gmail.com>
2021-05-14 19:25:16 +0500

    Добавлен тест для строки (Исправление ошибки)

--- src/test/java/testPack/MathRgrTest.java
+++ src/test/java/testPack/MathRgrTest.java
@@ -2,13 +2,27 @@ package testPack;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import Kurs13.MathRgr;
 
 public class MathRgrTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+@BeforeClass
+public static void setUpBeforeClass() throws Exception {
+}
+
+@Before
+public void setUp() throws Exception {
+
+}
+
+@Test
+public void test() {
+MathRgr math = new MathRgr();
+assertEquals(math.typrek,"You will see the selected ad type after the calculations");
+}
 
 }
