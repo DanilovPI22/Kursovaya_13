07e038792b72e47045add8eb52b5fef5c805f722
Kolesova222 <lera3262@gmail.com>
2021-05-14 19:22:35 +0500

    Добавлен тест для строки

---
+++ src/test/java/testPack/MathRgrTest.java
@@ -0,0 +1,14 @@
+package testPack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathRgrTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
